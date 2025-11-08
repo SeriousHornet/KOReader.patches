1ed5f319f098d80ebc7a1108c8ddb794606e41c2
Manu <64640197+SeriousHornet@users.noreply.github.com>
2025-11-08 22:17:31 +0800

    Update and rename

--- 2--disable-all-CB-widgets.lua
+++ 2--disable-all-CB-widgets.lua
@@ -1,8 +1,9 @@
 --[[ 
-User patch for KOReader: Disable specific UI elements
+User patch for KOReader to disable specific UI elements
 This patch disables:
 1. Progress bar
 2. Collections star
+3. Description hint (permanently)
 ]]--
 
 local userpatch = require("userpatch")
@@ -11,8 +12,9 @@ local function patchDisableUIElements(plugin)
     local ProgressWidget = require("ui/widget/progresswidget")
     local MosaicMenu = require("mosaicmenu")
     local ReadCollection = require("readcollection")
+    local BookInfoManager = require("bookinfomanager")
     
-    -- Disable progress bar and collection star
+    -- Disable progress bar, collection star, and description hint
     local MosaicMenuItem = userpatch.getUpValue(MosaicMenu._updateItemsBuildUI, "MosaicMenuItem")
 
     local orig_MosaicMenuItem_paint = MosaicMenuItem.paintTo
@@ -29,13 +31,23 @@ local function patchDisableUIElements(plugin)
         ReadCollection.isFileInCollections = function(filepath)
             return false
         end
+        
+        -- Permanently disable description hint by overriding the setting check
+        local orig_getSetting = BookInfoManager.getSetting
+        BookInfoManager.getSetting = function(self, setting_name)
+            if setting_name == "no_hint_description" then
+                return true
+            end
+            return orig_getSetting(self, setting_name)
+        end
     
         -- Call original paint method
         orig_MosaicMenuItem_paint(self, bb, x, y)
       
-        -- Restore original methods
+        -- Restore original methods (but keep description hint permanently disabled)
         ProgressWidget.paintTo = orig_ProgressWidget_paint
         ReadCollection.isFileInCollections = orig_isFileInCollections
+        BookInfoManager.getSetting = orig_getSetting
     end
 end
 
